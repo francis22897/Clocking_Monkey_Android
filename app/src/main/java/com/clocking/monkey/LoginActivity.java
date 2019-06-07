cc752f35d2ce8dd9bcdf56ce176efc37fa0c7fd8
francisco.adan <francisco.adan@escuelaestech.es>
2019-06-07 14:14:13 +0200

    Arreglado login

--- app/src/main/java/com/clocking/monkey/LoginActivity.java
+++ app/src/main/java/com/clocking/monkey/LoginActivity.java
@@ -3,6 +3,7 @@ package com.clocking.monkey;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.base.Strings;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText email, password;
     Button btnLogin, btnRegister;
     SharedPreferences prefs;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnRegister = findViewById(R.id.btn_register);
         email = findViewById(R.id.input_email);
         password = findViewById(R.id.input_password);
-        prefs = getSharedPreferences("UserData", Context.MODE_PRIVATE);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        prefs = getSharedPreferences("UserData", Context.MODE_PRIVATE);
 
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
@@ -41,33 +53,51 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_login:
-
                 if(Strings.isNullOrEmpty(email.getText().toString()) || Strings.isNullOrEmpty(password.getText().toString())){
                     Toast.makeText(this.getApplicationContext(), "No puedes dejar campos vacíos", Toast.LENGTH_LONG).show();
                 }else{
-                    if(FirebaseUtils.loginUser(email.getText().toString(), password.getText().toString())){
-
-                        String user = FirebaseUtils.searchUser(email.getText().toString());
-
-                        if(!Strings.isNullOrEmpty(user)){
-                            SharedPreferences.Editor editor = prefs.edit();
-                            editor.putString("user", user);
-                            editor.apply();
-
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
+                    firebaseAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                addCredentials();
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Fallo al realizar el logueo", Toast.LENGTH_LONG).show();
+                            }
                         }
-                    }
+                    });
                 }
-
                 break;
             case R.id.btn_register:
                 break;
         }
     }
 
+    public void addCredentials(){
+        firebaseFirestore.collection("Users").whereEqualTo("email", email.getText().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().getDocuments().size() > 0) {
+                        String email = task.getResult().getDocuments().get(0).getData().get("email").toString();
+                        String name = task.getResult().getDocuments().get(0).getData().get("name").toString();
+                        String first_lastname = task.getResult().getDocuments().get(0).getData().get("first_lastname").toString();
+                        String second_lastname = task.getResult().getDocuments().get(0).getData().get("second_lastname").toString();
 
-    public void showMessage(String message){
-        Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                        User user = new User(email, name, first_lastname, second_lastname);
+
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("user", user.toJson());
+                        editor.apply();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error al buscar usuario", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
