521676a8d303031b9a2cba0e1401a84bc8a51d4c
Juaniyo46 <juan.alvarez@escuelaestech.es>
2019-06-07 10:08:59 +0200

    añadiendo pantallas nfc a proyecto principal

---
+++ app/src/main/java/com/clocking/monkey/NFCActivity.java
@@ -0,0 +1,38 @@
+package com.clocking.monkey;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class NFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+        Toolbar toolbar = findViewById(R.id.toolbar_nfc);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_clockin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.back){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        return true;
+    }
+}
